6e28618dced30b36081f821ed52aabc3b17546b3
Cagatay Wilhelm <cagatayttnc@web.de>
2025-03-24 15:23:21 +0100

    started

---
+++ index.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+console.log(ctx);
+
+ctx.fillStyle = 'white';
+ctx.fillRect(50, 150, 100, 200);
+
+class Particle{
+
+};
+
+class Effect{
+
+};
+
+function animate(){
+
+};
