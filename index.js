4030e9b4a9f08d385c8847289068ab1d8d98106a
Cagatay Wilhelm <cagatayttnc@web.de>
2025-03-25 10:44:45 +0100

    starting

--- index.js
+++ index.js
@@ -4,15 +4,36 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 console.log(ctx);
 
-ctx.fillStyle = 'white';
-ctx.fillRect(50, 150, 100, 200);
+// ctx.fillStyle = 'white';
+// ctx.fillRect(150, 150, 150, 200);
 
 class Particle{
-
+    constructor(effect){
+        this.effect = effect;
+        this.x = Math.random() * this.effect.width;
+        this.y = Math.random() * this.effect.height;
+        this.radius = 15;
+    }
+    draw(context){
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.fill();
+    }
 };
 
 class Effect{
-
+    constructor(canvas){
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.particles = [];
+        this.numberOfParticles = 20;
+    }
+    createParticles(){
+        for (let i = 0; i < this.numberOfParticles; i++){
+            this.particles.push();
+        }
+    }
 };
 
 function animate(){
